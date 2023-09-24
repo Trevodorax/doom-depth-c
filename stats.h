fde84988095b5bf3aa076b0a23d26313b055fa1a
AEA <aea@myges.fr>
2023-09-24 22:00:34 +0200

    feat(all): create stats structure

---
+++ stats.h
@@ -0,0 +1,12 @@
+#ifndef STATS
+#define STATS
+
+typedef struct {
+    int nbMonstersKilled;
+    int nbDeaths;
+    int damagesDealed;
+    int healthHealed;
+    int maxLevelReached;
+} Stats;
+
+#endif
