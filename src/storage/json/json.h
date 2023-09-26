ff32ce8310fc8cb9dbd3d677f31381e67314e92e
Paul Gaudeaux <phvcgdx@gmail.com>
2023-09-26 14:58:59 +0200

    feat(json): create prototypes for json

--- src/storage/json/json.h
+++ src/storage/json/json.h
@@ -1,6 +1,42 @@
 #ifndef DOOM_DEPTH_C_JSON_H
 #define DOOM_DEPTH_C_JSON_H
 
+typedef struct Json Json;
+struct Json {
+    /**
+     * a: array
+     * o: object
+     * s: string
+     * n: number
+     */
+    char type;
+
+    // if number
+    int number;
+
+    // if string
+    char *string;
+
+    // if array or object
+    size_t nb_elements;
+    char **keys;
+    Json *values;
+};
+
+/**
+ * @brief Parses json file and stores it in a Json struct
+ *
+ * @param file_path Path of the file to parse. Should be a well formatted .json file. (doesn't take ownership)
+ * @return The parsed Json object (gives ownership)
+ * @errors Returns NULL in the following cases:
+ * - impossible to open file
+ * - bad .json format in file
+ * - cannot allocate memory
+ * @author Paul Gaudeaux
+ * @date 26/09/23
+ */
+Json *get_json_from_file(const char *file_path);
+
 int json_test();
 
 #endif //DOOM_DEPTH_C_JSON_H
