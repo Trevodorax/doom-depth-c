d3a1347beec4ac113be87e3809a95f27e9c8aea1
Tom BOURLARD <bourlard.tom.94@gmail.com>
2023-09-30 11:52:33 +0200

    fix(database): modify script to init db

--- src/storage/database/database_queries.c
+++ src/storage/database/database_queries.c
@@ -1,29 +1,92 @@
 #include "database_queries.h"
 
-const char create_armor_table_sql[] =
-        "CREATE TABLE IF NOT EXISTS ARMOR ("
+
+const char create_spell_table_sql[] =
+        "CREATE TABLE IF NOT EXISTS SPELL ("
         "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "type INTEGER NOT NULL,"
         "name TEXT NOT NULL,"
         "amount INTEGER NOT NULL,"
-        "rarity INTEGER NOT NULL,"
-        "max_uses INTEGER NOT NULL,"
-        "uses INTEGER NOT NULL,"
-        "cost INTEGER NOT NULL,"
-        "image TEXT NOT NULL"
+        "cost INTEGER NOT NULL"
+        ");";
+
+const char create_stats_table_sql[] =
+        "CREATE TABLE IF NOT EXISTS STATS ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "nb_monsters_killed INTEGER NOT NULL,"
+        "nb_deaths INTEGER NOT NULL,"
+        "damages_dealt INTEGER NOT NULL,"
+        "health_healed INTEGER NOT NULL,"
+        "max_level_reached INTEGER NOT NULL"
+        ");";
+
+const char create_player_table_sql[] =
+        "CREATE TABLE IF NOT EXISTS PLAYER ("
+        "    id                 INTEGER primary key autoincrement,"
+        "    name               TEXT    not null,"
+        "    hp                 INTEGER not null,"
+        "    hp_max             INTEGER not null,"
+        "    mana               INTEGER not null,"
+        "    mana_max           INTEGER not null,"
+        "    gold               INTEGER not null,"
+        "    xp                 INTEGER not null,"
+        "    level              INTEGER not null,"
+        "    base_attack        INTEGER not null,"
+        "    base_defense       INTEGER not null,"
+        "    offensive_spell_id INTEGER not null references SPELL,"
+        "    defensive_spell_id INTEGER not null references SPELL,"
+        "    healing_spell_id   INTEGER not null references SPELL,"
+        "    stats_id           INTEGER not null references STATS,"
+        "    inventory_capacity INTEGER not null,"
+        "    nb_weapons         INTEGER not null,"
+        "    nb_armors          INTEGER not null,"
+        "    nb_mana_potions    INTEGER not null,"
+        "    nb_health_potions  INTEGER not null,"
+        "    image              TEXT    not null"
+        ");";
+
+const char create_armor_table_sql[] =
+        "CREATE TABLE IF NOT EXISTS ARMOR ("
+        "id       INTEGER"
+        "        primary key autoincrement,"
+        "    name     TEXT    not null,"
+        "    amount   INTEGER not null,"
+        "    rarity   INTEGER not null,"
+        "    max_uses INTEGER not null,"
+        "    cost     INTEGER not null,"
+        "    image    TEXT    not null"
         ");";
 
 const char create_weapon_table_sql[] =
         "CREATE TABLE IF NOT EXISTS WEAPON ("
-        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "name TEXT NOT NULL,"
-        "min_attack INTEGER NOT NULL,"
-        "max_attack INTEGER NOT NULL,"
-        "attacks_per_turn INTEGER NOT NULL,"
-        "rarity INTEGER NOT NULL,"
-        "max_uses INTEGER NOT NULL,"
-        "uses INTEGER NOT NULL,"
-        "cost INTEGER NOT NULL,"
-        "image TEXT NOT NULL"
+        "    id               INTEGER"
+        "        primary key autoincrement,"
+        "    name             TEXT    not null,"
+        "    min_attack       INTEGER not null,"
+        "    max_attack       INTEGER not null,"
+        "    attacks_per_turn INTEGER not null,"
+        "    rarity           INTEGER not null,"
+        "    max_uses         INTEGER not null,"
+        "    cost             INTEGER not null,"
+        "    image            TEXT    not null"
+        ");";
+
+const char create_armors_in_inventory_table_sql[] =
+        "CREATE TABLE IF NOT EXISTS ARMORS_IN_INVENTORY ("
+        "    player_id INTEGER not null references PLAYER,"
+        "    armor_id     INTEGER not null references ARMOR,"
+        "    uses         INTEGER not null,"
+        "    chosen       INTEGER not null,"
+        "    primary key (player_id, armor_id)"
+        ");";
+
+const char create_weapons_in_inventory_table_sql[] =
+        "CREATE TABLE IF NOT EXISTS WEAPONS_IN_INVENTORY ("
+        "    player_id INTEGER not null references PLAYER,"
+        "    weapon_id     INTEGER not null references WEAPON,"
+        "    uses         INTEGER not null,"
+        "    chosen       INTEGER not null,"
+        "    primary key (player_id, weapon_id)"
         ");";
 
 const char create_monster_table_sql[] =
@@ -38,61 +101,6 @@ const char create_monster_table_sql[] =
         "image TEXT NOT NULL"
         ");";
 
-const char create_inventory_table_sql[] =
-        "CREATE TABLE IF NOT EXISTS INVENTORY ("
-        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "capacity INTEGER NOT NULL,"
-        "nb_weapons INTEGER NOT NULL,"
-        "nb_armors INTEGER NOT NULL,"
-        "nb_mana_potions INTEGER NOT NULL,"
-        "nb_health_potions INTEGER NOT NULL"
-        ");";
-
-const char create_armors_in_inventory_table_sql[] =
-        "CREATE TABLE IF NOT EXISTS ARMORS_IN_INVENTORY ("
-        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "inventory_id INTEGER NOT NULL REFERENCES INVENTORY,"
-        "armor_id INTEGER NOT NULL REFERENCES ARMOR"
-        ");";
-
-const char create_weapons_in_inventory_table_sql[] =
-        "CREATE TABLE IF NOT EXISTS WEAPONS_IN_INVENTORY ("
-        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "inventory_id INTEGER NOT NULL REFERENCES INVENTORY,"
-        "weapon_id INTEGER NOT NULL REFERENCES WEAPON"
-        ");";
-
-const char create_spell_table_sql[] =
-        "CREATE TABLE IF NOT EXISTS SPELL ("
-        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "type INTEGER NOT NULL,"
-        "name TEXT NOT NULL,"
-        "amount INTEGER NOT NULL,"
-        "cost INTEGER NOT NULL"
-        ");";
-
-const char create_player_table_sql[] =
-        "CREATE TABLE IF NOT EXISTS PLAYER ("
-        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "name TEXT NOT NULL,"
-        "hp INTEGER NOT NULL,"
-        "hp_max INTEGER NOT NULL,"
-        "mana INTEGER NOT NULL,"
-        "mana_max INTEGER NOT NULL,"
-        "gold INTEGER NOT NULL,"
-        "xp INTEGER NOT NULL,"
-        "level INTEGER NOT NULL,"
-        "base_attack INTEGER NOT NULL,"
-        "base_defense INTEGER NOT NULL,"
-        "offensive_spell_id INTEGER NOT NULL REFERENCES SPELL,"
-        "defensive_spell_id INTEGER NOT NULL REFERENCES SPELL,"
-        "healing_spell_id INTEGER NOT NULL REFERENCES SPELL,"
-        "inventory_id INTEGER NOT NULL REFERENCES INVENTORY,"
-        "chosen_weapon_id INTEGER NOT NULL REFERENCES WEAPON,"
-        "chosen_armor_id INTEGER NOT NULL REFERENCES ARMOR,"
-        "stats_id INTEGER NOT NULL REFERENCES STATS"
-        ");";
-
 const char create_stage_table_sql[] =
         "CREATE TABLE IF NOT EXISTS STAGE ("
         "id INTEGER PRIMARY KEY AUTOINCREMENT,"
@@ -100,34 +108,22 @@ const char create_stage_table_sql[] =
         "enemy_chances_to_appear TEXT NOT NULL"
         ");";
 
-
-const char create_stats_table_sql[] =
-        "CREATE TABLE IF NOT EXISTS STATS ("
-        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "nb_monsters_killed INTEGER NOT NULL,"
-        "nb_deaths INTEGER NOT NULL,"
-        "damages_dealt INTEGER NOT NULL,"
-        "health_healed INTEGER NOT NULL,"
-        "max_level_reached INTEGER NOT NULL"
-        ");";
-
-
 const char insert_armor_sql[] =
-        "INSERT INTO ARMOR (name, amount, rarity, max_uses, uses, cost, image)"
-        "VALUES ('Leather Armor', 1, 1, 10, 10, 10, '../../../assets/leather_armor.png'),"
-        "('Chainmail', 1, 1, 20, 20, 20, '../../../assets/chainmail.png'),"
-        "('Plate Armor', 1, 2, 30, 30, 30, '../../../assets/plate_armor.png'),"
-        "('Dragon Armor', 1, 2, 40, 40, 40, '../../../assets/dragon_armor.png'),"
-        "('God Armor', 1, 3, 50, 50, 50, '../../../assets/god_armor.png');";
+        "INSERT INTO ARMOR (name, amount, rarity, max_uses, cost, image)"
+        "VALUES ('Leather Armor', 1, 1, 10, 10, '../../../assets/leather_armor.png'),"
+        "       ('Chainmail', 1, 1, 20, 20, '../../../assets/chainmail.png'),"
+        "       ('Plate Armor', 1, 2, 30, 30, '../../../assets/plate_armor.png'),"
+        "       ('Dragon Armor', 1, 2, 40, 40, '../../../assets/dragon_armor.png'),"
+        "       ('God Armor', 1, 3, 50, 50, '../../../assets/god_armor.png');";
 
 const char insert_weapon_sql[] =
-        "INSERT INTO WEAPON (name, min_attack, max_attack, attacks_per_turn, rarity, max_uses, uses, cost, image)"
-        "VALUES ('Butter Knife', 1, 2, 1, 1, 10, 10, 3, '../../../assets/butter_knife.png'),"
-        "('Compass', 2, 4, 1, 1, 10, 10, 5, '../../../assets/compass.png'),"
-        "('Wooden Sword', 3, 6, 1, 2, 10, 10, 10, '../../../assets/wooden_sword.png'),"
-        "('Lightsaber', 4, 8, 1, 2, 10, 10, 15, '../../../assets/lightsaber.png'),"
-        "('Flip-Flops', 5, 10, 1, 3, 10, 10, 20, '../../../assets/flip_flops.png'),"
-        "('Wand', 6, 12, 1, 3, 10, 10, 25, '../../../assets/wand.png');";
+        "INSERT INTO WEAPON (name, min_attack, max_attack, attacks_per_turn, rarity, max_uses, cost, image)"
+        "VALUES ('Butter Knife', 1, 2, 1, 1, 10, 3, '../../../assets/butter_knife.png'),"
+        "       ('Compass', 2, 4, 1, 1, 10, 5, '../../../assets/compass.png'),"
+        "       ('Wooden Sword', 3, 6, 1, 2, 10, 10, '../../../assets/wooden_sword.png'),"
+        "       ('Lightsaber', 4, 8, 1, 2, 10, 15, '../../../assets/lightsaber.png'),"
+        "       ('Flip-Flops', 5, 10, 1, 3, 10, 20, '../../../assets/flip_flops.png'),"
+        "       ('Wand', 6, 12, 1, 3, 10, 25, '../../../assets/wand.png');";
 
 const char insert_spell_sql[] =
         "INSERT INTO SPELL (type, name, amount, cost) VALUES (1, 'Heal', 10, 10),"
@@ -165,11 +161,6 @@ Table_Info all_tables_info[] = {
                 "SELECT name FROM sqlite_master WHERE type='table' AND name='MONSTER';",
                 create_monster_table_sql,
                 insert_monster_sql
-        },
-        {
-                "SELECT name FROM sqlite_master WHERE type='table' AND name='INVENTORY';",
-                create_inventory_table_sql,
-                NULL
         },
         {
                 "SELECT name FROM sqlite_master WHERE type='table' AND name='ARMORS_IN_INVENTORY';",
--- src/storage/database/database_queries.h
+++ src/storage/database/database_queries.h
@@ -14,7 +14,6 @@ typedef struct Table_Info {
 extern const char create_armor_table_sql[];
 extern const char create_weapon_table_sql[];
 extern const char create_monster_table_sql[];
-extern const char create_inventory_table_sql[];
 extern const char create_armors_in_inventory_table_sql[];
 extern const char create_weapons_in_inventory_table_sql[];
 extern const char create_spell_table_sql[];
