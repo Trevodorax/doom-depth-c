80da2e52a90fe586fad14cd5ca222ad77759e09a
Tom-Brd <bourlard.tom.94@gmail.com>
2023-10-05 23:34:36 +0200

    feat(monstersStruct): change monster's struct creation function

--- src/entities/monster/monster.c
+++ src/entities/monster/monster.c
@@ -1,48 +1,23 @@
 #include "monster.h"
 
-array_node_t *create_monsters_from_db(sqlite3 *db) {
+void *create_monster_from_db(sqlite3_stmt *stmt) {
 
-    const char *query = "SELECT * FROM MONSTER";
+    monster_t *monster = malloc(sizeof(monster_t));
 
-    sqlite3_stmt *stmt;
-    int rc = sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
-
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+    if (!monster) {
+        fprintf(stderr, "Failed to allocate memory for monster.\n");
         return NULL;
     }
 
-    array_node_t *head = NULL;
+    monster->id = sqlite3_column_int(stmt, 0);
+    monster->type = (short)sqlite3_column_int(stmt, 1);
+    monster->name = strdup((char *) sqlite3_column_text(stmt, 2));
+    monster->hp = sqlite3_column_int(stmt, 3);
+    monster->hp_max = sqlite3_column_int(stmt, 4);
+    monster->attack = sqlite3_column_int(stmt, 5);
+    monster->defense = sqlite3_column_int(stmt, 6);
+    monster->image = strdup((char *) sqlite3_column_text(stmt, 7));
 
-    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-
-        monster_t *monster = malloc(sizeof(monster_t));
-
-        if (!monster) {
-            fprintf(stderr, "Failed to allocate memory for monster.\n");
-            sqlite3_finalize(stmt);
-            return NULL;
-        }
-
-        monster->id = sqlite3_column_int(stmt, 0);
-        monster->type = (short)sqlite3_column_int(stmt, 1);
-        monster->name = strdup((char *) sqlite3_column_text(stmt, 2));
-        monster->hp = sqlite3_column_int(stmt, 3);
-        monster->hp_max = sqlite3_column_int(stmt, 4);
-        monster->attack = sqlite3_column_int(stmt, 5);
-        monster->defense = sqlite3_column_int(stmt, 6);
-        monster->image = strdup((char *) sqlite3_column_text(stmt, 7));
-
-        append(&head, monster, sizeof(monster_t));
-    }
-
-    if (rc != SQLITE_DONE) {
-        fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
-        sqlite3_finalize(stmt);
-        return NULL;
-    }
-
-    sqlite3_finalize(stmt);
-    return head;
+    return monster;
 
 }
--- src/entities/monster/monster.h
+++ src/entities/monster/monster.h
@@ -2,6 +2,7 @@
 #define MONSTER
 
 #include "../../utils/array.h"
+#include "../../storage/database/database.h"
 #include <sqlite3.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,19 +19,19 @@ typedef struct {
 } monster_t;
 
 /**
- * @brief Creates a linked list of monsters from the database.
+ * @brief Creates a monster from the database.
  *
- * This function queries the database for all monsters and constructs a linked list
- * where each node represents a monster with details fetched from the database.
+ * This function queries the database for a monster with the given ID and constructs
+ * a monster with details fetched from the database.
  *
- * @param db  The SQLite database connection which contains the monsters' data.
- * @return   Returns a pointer to the head of the linked list containing monsters,
- *           or NULL if there was an error or no monsters were found.
+ * @param stmt  The SQLite statement which contains the monster's data.
+ * @return   Returns a pointer to the monster with the given ID, or NULL if there was an error
+ *           or no monster was found.
  *
- * @sideeffects Allocates memory for the linked list and its nodes.
- * @dependencies Depends on the SQLite library and the provided linked list functions.
+ * @sideeffects Allocates memory for the monster.
+ * @dependencies Depends on the SQLite library.
  * @errors Errors during database operations or memory allocation can result in NULL being returned.
  */
-array_node_t *create_monsters_from_db(sqlite3 *db);
+void *create_monster_from_db(sqlite3_stmt *stmt);
 
 #endif
