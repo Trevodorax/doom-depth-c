68e0185776040e8891df0aff0ee5de7e8b2e51d6
Tom-Brd <bourlard.tom.94@gmail.com>
2023-10-18 14:31:28 +0200

    feat(stats): function to save stats in DB

--- src/entities/stats/stats.c
+++ src/entities/stats/stats.c
@@ -29,3 +29,24 @@ void *create_stats_from_db(sqlite3_stmt *stmt) {
     return stats;
 
 }
+
+int save_stats(sqlite3 *db, stats_t *stats, int player_id) {
+
+    char *z_err_msg = NULL;
+    sqlite3_stmt *stmt;
+
+    char *sql = malloc(sizeof(char) * 150);
+    sprintf(sql, "UPDATE STATS SET nb_monsters_killed = %d, nb_deaths = %d, damages_dealt = %d, health_healed = %d, max_level_reached = %d WHERE id = %d", stats->nb_monsters_killed, stats->nb_deaths, stats->damages_dealt, stats->health_healed, stats->max_level_reached, player_id);
+
+    int rc = sqlite3_exec(db, sql, NULL, 0, &z_err_msg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", z_err_msg);
+        sqlite3_free(z_err_msg);
+        return 1;
+    }
+
+    free(sql);
+    return 0;
+
+}
--- src/entities/stats/stats.h
+++ src/entities/stats/stats.h
@@ -28,6 +28,34 @@ typedef struct {
 */
 stats_t *create_stats();
 
+/**
+ * @brief Creates a stats struct from the database.
+ *
+ * This function allocates memory for a new stats struct and initializes its fields with the values from the database.
+ *
+ * @param stmt
+ * @return
+ *
+ * @sideeffects Allocates memory for the stats.
+ * @dependencies None.
+ * @errors None.
+ */
 void *create_stats_from_db(sqlite3_stmt *stmt);
+
+/**
+ * @brief Saves the stats of a player in the database.
+ *
+ * This function updates the stats of a player in the database.
+ *
+ * @param db
+ * @param stats
+ * @param player_id
+ * @return
+ *
+ * @sideeffects None.
+ * @dependencies None.
+ * @errors None.
+ */
+int save_stats(sqlite3 *db, stats_t *stats, int player_id);
 
 #endif
