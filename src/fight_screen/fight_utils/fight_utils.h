e7875bf0b71a7ac30e3c08e53c9cc707746421e9
Paul Gaudeaux <phvcgdx@gmail.com>
2023-10-23 16:41:33 +0200

    chores(fight): add missing documentation on utils functions

--- src/fight_screen/fight_utils/fight_utils.h
+++ src/fight_screen/fight_utils/fight_utils.h
@@ -14,18 +14,55 @@
  */
 unsigned int player_attack(player_t * player, monster_t * target);
 
-fight_context_t * build_fight_context(fight_t * fight, player_t * player);
-
+/**
+ * @brief Sets a notification message for a fight and prints it.
+ *
+ * @param fight_context The fight context in which the notification is displayed.
+ * @param message The notification message to set and display.
+ */
 void build_notification(fight_context_t * fight_context, char * message);
 
+/**
+ * @brief Sets a formatted notification message for a fight and prints it.
+ *
+ * @param fight_context The fight context in which the notification is displayed.
+ * @param message The formatted message string.
+ * @param ... Variable arguments for message formatting.
+ */
 void build_notification_formatted(fight_context_t * fight_context, char * message, ...);
 
+/**
+ * @brief Finds the index in a probability array based on a random number.
+ *
+ * @param rand_num The random number generated.
+ * @param probs The array of probabilities.
+ * @param size The size of the probability array.
+ * @return The index corresponding to the random number or -1 if no match is found.
+ */
 int find_index(int rand_num, const int probs[], int size);
 
+/**
+ * @brief Fetches a monster based on its name. (Temporary function)
+ *
+ * @param name The name of the monster.
+ * @return A pointer to the monster struct or NULL if no match is found.
+ */
 void * get_monster_with_name(char * name);
 
+/**
+ * @brief Displays the current and maximum action points of a player.
+ *
+ * @param player The player whose action points are to be displayed.
+ */
 void debug_action_points(player_t *player);
 
+/**
+ * @brief Checks if a player has sufficient action points and deducts the required amount if possible.
+ *
+ * @param player The player whose action points are being checked.
+ * @param amount The amount of action points required.
+ * @return Returns true if the player has enough action points and they are deducted, false otherwise.
+ */
 bool check_and_remove_action_points(player_t *player, unsigned int amount);
 
 #endif //DOOM_DEPTH_C_FIGHT_UTILS_H
