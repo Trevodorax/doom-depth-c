b606f41cd170392f11585cc532ddadab8b030683
aea <aea@myges.fr>
2023-10-26 23:31:57 +0200

    fix(shop): go back to map screen when exiting shop

--- src/shop_screen/shop_screen.h
+++ src/shop_screen/shop_screen.h
@@ -4,6 +4,13 @@
 #include "../game_window/game_window.h"
 #include "../entities/player/player.h"
 
+/**
+ * @brief Displays the shop screen instead of whatever was showing before
+ *
+ * @param game_window the window to use
+ * @param player the player currently playing
+ * @return EXIT_FAILURE or EXIT_SUCCESS
+ */
 int shop_screen(game_window_t * game_window, player_t ** player);
 
 #endif
