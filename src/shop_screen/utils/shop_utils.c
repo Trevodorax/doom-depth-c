2fef61a3aa2d656fb6a2d93b5f94f095d2c75389
aea <aea@myges.fr>
2023-10-26 17:20:17 +0200

    feat(shop): create utils to get item details

--- src/shop_screen/utils/shop_utils.c
+++ src/shop_screen/utils/shop_utils.c
@@ -1,6 +1,7 @@
 #include "shop_utils.h"
 #include "../../utils/array.h"
 #include "../../utils/utils.h"
+#include <stdlib.h>
 
 int get_items_count(category_options category) {
     switch (category) {
@@ -24,9 +25,123 @@ int get_items_count(category_options category) {
     }
 }
 
-char *player_gold_to_string(player_t *player) {
+char * player_gold_to_string(player_t * player) {
     char *res = malloc(sizeof(char) * 10);
     res[0] = '\0';
     custom_itoa((int) player->gold, res, 10);
+    return res;
+}
+
+char * shop_armor_details_to_string(armor_t * armor) {
+    char buffer[4];
+
+    char *res = malloc(sizeof(char) * 1000);
+    res[0] = '\0';
+
+    strcat(res, "Name : ");
+    strcat(res, custom_strupr(armor->name));
+    strcat(res, "\n\n");
+
+    strcat(res, "Rarity : ");
+    strcat(res, custom_itoa((int) armor->rarity, buffer, 10));
+    strcat(res, "\n\n");
+
+    strcat(res, "Defense value : ");
+    strcat(res, custom_itoa((int) armor->amount, buffer, 10));
+    strcat(res, "\n\n");
+
+    strcat(res, "Uses : ");
+    strcat(res, custom_itoa((int) armor->max_uses, buffer, 10));
+    strcat(res, "\n\n");
+
+    strcat(res, "Cost : ");
+    strcat(res, custom_itoa((int) armor->cost, buffer, 10));
+    strcat(res,"\n");
+
+    res = realloc(res, sizeof(char) * (strlen(res) + 1));
+    if (!res) {
+        return NULL;
+    }
+
+    return res;
+}
+
+char *shop_weapon_details_to_string(weapon_t *weapon) {
+    char buffer[4];
+
+    char *res = malloc(sizeof(char) * 1000);
+    res[0] = '\0';
+
+    strcat(res, "Name : ");
+    strcat(res, custom_strupr(weapon->name));
+    strcat(res, "\n\n");
+
+    strcat(res, "Rarity : ");
+    strcat(res, custom_itoa((int) weapon->rarity, buffer, 10));
+    strcat(res, "\n\n");
+
+    strcat(res, "Attacks per turn : ");
+    strcat(res, custom_itoa((int) weapon->attacks_per_turn, buffer, 10));
+    strcat(res, "\n\n");
+
+    strcat(res, "Attack range : ");
+    strcat(res, custom_itoa((int) weapon->min_attack, buffer, 10));
+    strcat(res, " - ");
+    strcat(res, custom_itoa((int) weapon->max_attack, buffer, 10));
+    strcat(res, "\n\n");
+
+    strcat(res, "Uses : ");
+    strcat(res, custom_itoa((int) weapon->max_uses, buffer, 10));
+    strcat(res, "\n\n");
+
+    strcat(res, "Cost : ");
+    strcat(res, custom_itoa((int) weapon->cost, buffer, 10));
+    strcat(res,"\n");
+
+    res = realloc(res, sizeof(char) * (strlen(res) + 1));
+    if (!res) {
+        return NULL;
+    }
+
+    return res;
+}
+
+char *shop_health_potions_details_to_string() {
+    char buffer[4];
+
+    char *res = malloc(sizeof(char) * 1000);
+    res[0] = '\0';
+
+    strcat(res, "Name : HEALTH POTION\n\n");
+    strcat(res, "Healing power : 20\n\n");
+    strcat(res, "Cost : ");
+    strcat(res, custom_itoa(HEALTH_POTIONS_COST, buffer, 10));
+    strcat(res,"\n");
+
+    res = realloc(res, sizeof(char) * (strlen(res) + 1));
+    if (!res) {
+        return NULL;
+    }
+
+    return res;
+}
+
+char *shop_mana_potions_details_to_string() {
+    char buffer[4];
+
+    char *res = malloc(sizeof(char) * 1000);
+    res[0] = '\0';
+
+    strcat(res, "Name : MANA POTION\n\n");
+    strcat(res, "Mana value : 20\n\n");
+    strcat(res, "Cost : ");
+    strcat(res, custom_itoa(MANA_POTIONS_COST, buffer, 10));
+    strcat(res,"\n");
+
+    res = realloc(res, sizeof(char) * (strlen(res) + 1));
+    if (!res) {
+        return NULL;
+    }
+
     return res;
 }
--- src/shop_screen/utils/shop_utils.h
+++ src/shop_screen/utils/shop_utils.h
@@ -1,6 +1,9 @@
 #ifndef DOOM_DEPTH_C_SHOP_UTILS_H
 #define DOOM_DEPTH_C_SHOP_UTILS_H
 
+#define HEALTH_POTIONS_COST 10
+#define MANA_POTIONS_COST 20
+
 #include "../shop_screen.h"
 #include "stdbool.h"
 #include "../../utils/items_management/types.h"
@@ -14,10 +17,41 @@
 int get_items_count(category_options category);
 
 /**
- * @brief Returns the quantity of gold a player has in a string
+ * @brief Converts the quantity of gold a player has into a string
  *
  * @param player
+ * @returns the quantity of gold a player has in a string
  */
-char *player_gold_to_string(player_t *player);
+char * player_gold_to_string(player_t * player);
+
+/**
+ * @brief Converts the details of an armor (useful for the shop) into a string
+ *
+ * @param armor
+ * @returns the details of given armor in a string
+ */
+char * shop_armor_details_to_string(armor_t * armor);
+
+/**
+ * @brief Converts the details of a weapon (useful for the shop) into a string
+ *
+ * @param weapon
+ * @returns the details of given armor in a string
+ */
+char * shop_weapon_details_to_string(weapon_t * weapon);
+
+/**
+ * @brief Converts the details of a health potion (useful for the shop) into a string
+ *
+ * @returns the details in a string
+ */
+char *shop_health_potions_details_to_string();
+
+/**
+ * @brief Converts the details of a health potion (useful for the shop) into a string
+ *
+ * @returns the details in a string
+ */
+char *shop_mana_potions_details_to_string();
 
 #endif //DOOM_DEPTH_C_SHOP_UTILS_H
