45852da9ab62f88d8f28ef12eb52130ed42b9127
Tom-Brd <bourlard.tom.94@gmail.com>
2023-10-30 20:58:50 +0100

    chores(loadGameDisplay): add doxygen to function

--- src/load_game_screen/display/display.h
+++ src/load_game_screen/display/display.h
@@ -6,6 +6,20 @@
 #include "../../ui_utils/ui_utils.h"
 #include "../../utils/array.h"
 
+/**
+ * @brief Displays the load game screen.
+ *
+ * This function displays the load game screen.
+ *
+ * @param game_window A pointer to the game window.
+ * @param players A pointer to the array of players.
+ * @param active_option The index of the active option.
+ *
+ * @return EXIT_SUCCESS.
+ * @sideeffects May modify the SDL renderer by rendering new elements.
+ * @dependencies Depends on the SDL2 library.
+ * @errors None.
+ */
 int display_load_game(game_window_t *game_window, array_node_t *players, unsigned short active_option);
 
 #endif
