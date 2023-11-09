a71d94b0504c4d890b79720892d2737e6cbf7f2c
aea <aea@myges.fr>
2023-11-09 23:47:52 +0100

    feat(confirm-quit): add documentation

--- src/confirm_quit_screen/display/display.h
+++ src/confirm_quit_screen/display/display.h
@@ -3,6 +3,13 @@
 
 #include "../../game_window/game_window.h"
 
+/**
+ * @brief Displays the quit confirmation screen instead of what was previously there and starts the suit confirmation event loop
+ *
+ * @param game_window
+ * @param quit The currently selected option
+ * @return QUIT_GAME or -1
+ */
 int display_confirm_quit(game_window_t * game_window, bool quit);
 
 #endif //DOOM_DEPTH_C_CONFIRM_QUIT_DISPLAY_H
